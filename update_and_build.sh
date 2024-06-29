fdf8e1ec6a9380e5f78f615c6146e0f292b6c45b
David Aryasetia <david.aryasetia@gmail.com>
2024-06-29 15:03:21 +0700

    update flutter build web --web-renderer html --release

--- update_and_build.sh
+++ update_and_build.sh
@@ -3,7 +3,7 @@ rm -rf ./build/web/*
 rm -rf ./public_html/*
 
 # Compile flutter to build 
-flutter build web
+flutter build web --web-renderer html --release
 
 # Copy Project to public html
 cp -r ./build/web/* ./public_html
