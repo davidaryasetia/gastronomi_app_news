e10cc7541e7a66d5239da37046ac3e4c64b13c91
David Aryasetia <david.aryasetia@gmail.com>
2024-07-15 13:03:25 +0700

    update and build

--- public_html/flutter_bootstrap.js
+++ public_html/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1295947563"
+    serviceWorkerVersion: "2909537716"
   }
 });
--- public_html/flutter_service_worker.js
+++ public_html/flutter_service_worker.js
@@ -148,7 +148,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "93acc69ef0e9b64f65530bd987107879
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "bbf2d19c8c229595ef7385bbb1400f4b",
+"flutter_bootstrap.js": "039891e3d448454b2069c2098a6d0da4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
