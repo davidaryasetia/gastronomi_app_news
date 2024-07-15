3819fd07b2db2d16b1a6add1eec2e5e0e27d977f
David Aryasetia <david.aryasetia@gmail.com>
2024-07-15 15:53:36 +0700

    update build testing 1

--- public_html/flutter_service_worker.js
+++ public_html/flutter_service_worker.js
@@ -148,7 +148,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "93acc69ef0e9b64f65530bd987107879
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "f896b949b212a9ec4be717cf75b94e91",
+"flutter_bootstrap.js": "0f496ebd64525e26200d6efa4a2fe348",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
