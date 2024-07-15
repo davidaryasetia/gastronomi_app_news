3b19b2f1b5efcca98777b64c6ebd8fbbf759b604
David Aryasetia <david.aryasetia@gmail.com>
2024-07-16 00:18:51 +0700

    building app

--- building_application/flutter_service_worker.js
+++ building_application/flutter_service_worker.js
@@ -148,7 +148,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "93acc69ef0e9b64f65530bd987107879
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "38a6601f5652327fd1e451fe3dfb6677",
+"flutter_bootstrap.js": "326ac7d856dacc06060fa7d939157540",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
--- update_and_build.sh
+++ update_and_build.sh
@@ -1,11 +1,11 @@
 # Hapus semua bagian directory public_html dan build web
 rm -rf ./build/web/*
-rm -rf ./public_html/*
+rm -rf ./building_application/*
 
 mkdir building_application
 
 # Compile flutter to build 
-flutter build web --web-renderer html
+flutter build web --web-renderer canvaskit
 
 # Copy Project to public html
 cp -r ./build/web/* ./building_application
